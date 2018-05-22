163ad39b08fd2f78ab9b8da2488fd5805c38bff9
Nundy <18710939328@163.com>
2018-05-22 15:25:47 +0800

    done v1

--- src/server/config/mysql.js
+++ src/server/config/mysql.js
@@ -4,7 +4,7 @@ const db_fem = mysql.createConnection({
         user: 'root',               // 用户名
         password: '',               // 密码
         database: 'trading',            // 数据库名称
-        port: 3306,                   // 端口号
+        port: 3306,                   // 数据库端口号
         dateStrings: true          // 时间以字符串形式显示，否则会有类似这样的显示：Thu Apr 14 2016 00:00:00 GMT+0800 (中国标准时间) 17:20:12
 });
 module.exports = db_fem;
